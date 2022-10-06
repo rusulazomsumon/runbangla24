87932a5906afef73dddf9a450c4c63fb051623ed
sumon <18103020@iubat.edu>
2022-10-06 19:25:49 +0600

    menu erro updated

--- wp-content/themes/runbangla24/template-home.php
+++ wp-content/themes/runbangla24/template-home.php
@@ -10,6 +10,11 @@
 
     <!-- Main News Slider Start -->
     <div class="container-fluid">
+        <!-- ###############################Breaking News #################################### -->
+        <?php 
+            get_template_part('template-parts/pertials/content','nHeading'); 
+        ?>
+        <!-- Breaking News End -->
         <div class="row">
             <div class="col-lg-7 px-0">
                 <div class="owl-carousel main-carousel position-relative">
@@ -62,49 +67,8 @@
     <!-- Main News Slider End -->
 
 
-    <!-- Breaking News Start -->
-    <div class="container-fluid bg-dark py-3 mb-3">
-        <div class="container">
-            <div class="row align-items-center bg-dark">
-                <div class="col-12">
-                    <div class="d-flex justify-content-between">
-                        <div class="bg-primary text-dark text-center font-weight-medium py-2" style="width: 170px;">তাজা খবর</div>
-                            <div class="owl-carousel tranding-carousel position-relative d-inline-flex align-items-center ml-3"
-                                style="width: calc(100% - 170px); padding-right: 90px;">
-                                <!-- breking news title: dynamicfication -->
-                                <?php 
-                                        
-                                        $args = array(
-                                            'post_type' => 'post',
-                                            'order' => 'DSC',
-                                            'posts_per_page' => 3,
-                                            'category_name' => 'তাজা-খবর'
-
-                                        );
-                                        $query = new WP_Query($args);
-                                        while($query -> have_posts()){
-                                            $query->the_post();
-                                            ?>
-                                    <!-- Breaking news markup start -->
-                                <div class="text-truncate">
-                                    <a class="text-white text-uppercase font-weight-semi-bold" 
-                                        href="<?php the_permalink(); ?>"><?php the_title(); ?>
-                                    </a>
-                                    
-                                </div>
-                                <!-- breking news markup end -->
-                                    <!-- rest of loop -->
-                                    <?php
-                                        }
-                                        wp_reset_postdata();
-                                    ?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <!-- ###############################Breaking News #################################### -->
+    <?php get_template_part('template-parts/pertials/content','bknews'); ?>
     <!-- Breaking News End -->
 
 
