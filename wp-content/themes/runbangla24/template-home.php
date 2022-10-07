6977ce71aaf0375ba2340ad512ca9fced513666f
sumon <18103020@iubat.edu>
2022-10-07 20:20:14 +0600

    plugin updated

--- wp-content/themes/runbangla24/template-home.php
+++ wp-content/themes/runbangla24/template-home.php
@@ -425,18 +425,14 @@
 
                     <!-- Newsletter Start -->
                     <div class="mb-3">
-                        <div class="section-title mb-0">
-                            <h4 class="m-0 text-uppercase font-weight-bold">Newsletter</h4>
-                        </div>
                         <div class="bg-white text-center border border-top-0 p-3">
-                            <p>Aliqu justo et labore at eirmod justo sea erat diam dolor diam vero kasd</p>
+                            <p>নিয়মিত সর্বশেষ সংবাদ পেতে ইমেল দিয়ে সাবস্ক্রাইব করুন!</p>
                             <div class="input-group mb-2" style="width: 100%;">
                                 <input type="text" class="form-control form-control-lg" placeholder="Your Email">
                                 <div class="input-group-append">
-                                    <button class="btn btn-primary font-weight-bold px-3">Sign Up</button>
+                                    <button class="btn btn-primary font-weight-bold px-3">নিবন্ধন করুন</button>
                                 </div>
                             </div>
-                            <small>Lorem ipsum dolor sit amet elit</small>
                         </div>
                     </div>
                     <!-- Newsletter End -->
